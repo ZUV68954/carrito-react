9821da376379eb42221c55ffb389b7833f920de3
ZUV68954 <ZUV68954@educastur.es>
2024-12-02 21:02:12 +0100

    Añadido un estilo de botón (1h).

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import {AwesomeButton} from "react-awesome-button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [articulos, setArticulos] = useState([]);
+    const [carrito, setCarrito] = useState([]);
+    const [showCart, setShowCart] = useState(false);
+
+    // Carga los artículos desde el archivo JSON
+    useEffect(() => {
+        fetch('/articulos_navideños.json')
+            .then((response) => response.json())
+            .then((data) => setArticulos(data))
+    }, []);
+
+    // Añade el artículo al carrito
+    const añadirAlCarrito = (articulo) => setCarrito([...carrito, articulo]);
+
+    // Muestra y oculta el carrito
+    const toggleCart = () => setShowCart(!showCart);
+
+    return (
+        <div>
+            <h1 style={{marginLeft: '2px'}}>Artículos Navideños</h1>
+
+            {/* Botón para mostrar el carrito */}
+
+            <AwesomeButton style={{ marginLeft: '1%'}} type={"youtube"} onPress={toggleCart}>
+                Carrito de Compras
+            </AwesomeButton>
+
+
+            {/* Lista de artículos */}
+            <ul>
+                {articulos.map((articulo) => (<li key={articulo.id}>
+                        <h3>{articulo.nombre}</h3>
+                        <p>{articulo.descripcion}</p>
+                        <p>Precio: {articulo.precio}€</p>
+                        <AwesomeButton type={"secondary"} onPress={() => añadirAlCarrito(articulo)}>Añadir al Carrito</AwesomeButton>
+                    </li>))}
+            </ul>
+
+            {/* Pop-up del carrito */}
+            {showCart && (<div style={popupStyle}>
+                    <h2>Carrito de Compras</h2>
+                    <ul>
+                        {carrito.map((item, index) => (<li key={index}>
+                                {item.nombre} - {item.precio}€
+                            </li>))}
+                    </ul>
+                    <AwesomeButton  onPress={toggleCart}>Cerrar</AwesomeButton>
+                </div>)}
+        </div>);
 }
+
+// Estilo para el pop-up
+const popupStyle = {
+    position: 'fixed', top: '40%', left: '40%', padding: '20px', border: '1px solid #ddd',
+};
 
 export default App;
